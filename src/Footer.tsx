4ad72987521197164737a90a25f49e17da61586a
Haaxor1689 <betkomaros@gmail.com>
2026-02-08 23:51:54 +0100

    Google ads and footer update

---
+++ src/Footer.tsx
@@ -0,0 +1,28 @@
+import { Bug } from 'lucide-react';
+
+import TextButton from './components/TextButton.tsx';
+
+const Footer = async () => (
+	<footer className="text-blue-gray flex max-w-7xl items-center justify-center gap-1 p-2 md:px-6 md:py-2 xl:mx-auto xl:w-full">
+		<p className="text-blue-gray pr-2 text-sm">Created by Haaxor1689</p>•
+		<TextButton
+			type="link"
+			href="https://discord.gg/pDeTHQH99B"
+			icon={Bug}
+			iconSize={18}
+			className="text-blue-gray shrink-0 text-sm"
+		>
+			Report a bug
+		</TextButton>
+		•
+		<TextButton
+			type="link"
+			href="https://haaxor1689.dev/privacy-policy"
+			className="text-blue-gray shrink-0 text-sm"
+		>
+			Privacy Policy
+		</TextButton>
+	</footer>
+);
+
+export default Footer;
